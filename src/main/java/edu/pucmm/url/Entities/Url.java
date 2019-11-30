33f26503b8463e71c1c3cac544b0e8fc7058f21d
Jose <jgg032899@gmail.com>
2019-11-30 12:50:59 -0400

    Minor changes

--- src/main/java/edu/pucmm/url/Entities/Url.java
+++ src/main/java/edu/pucmm/url/Entities/Url.java
@@ -1,8 +1,6 @@
 package edu.pucmm.url.Entities;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
