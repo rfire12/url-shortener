c10d1deaf9c0b32b3d0868e385539e60ee68b0c1
José Gabriel González <jgg032899@gmail.com>
2019-12-06 10:40:01 -0400

    Update UrlWebService.java

--- src/main/java/edu/pucmm/url/Soap/UrlWebService.java
+++ src/main/java/edu/pucmm/url/Soap/UrlWebService.java
@@ -2,10 +2,12 @@ package edu.pucmm.url.Soap;
 
 import edu.pucmm.url.Entities.Url;
 import edu.pucmm.url.Entities.User;
+import edu.pucmm.url.Services.UrlServices;
 import edu.pucmm.url.Services.UsersServices;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,11 +24,16 @@ public class UrlWebService {
         return user.getMyUrls();
     }
 
-//    @WebMethod
-//    public Url getShortUrl(String original, String username) {
-//        User user = UsersServices.getInstance().findByUsername(username);
-//        String shortUrl = UUID.randomUUID().toString().split("-")[0];
-//        Url url = new Url(shortUrl, original, "", user, "");
-//        return url;
-//    }
+    @WebMethod
+    public Url getShortUrl(String original, String username) {
+        User user = UsersServices.getInstance().findByUsername(username);
+        String shortUrl = UUID.randomUUID().toString().split("-")[0];
+        Url url = new Url(shortUrl, original, "", user, "");
+        UrlServices.getInstance().create(url);
+        List<Url> myUrls = user.getMyUrls();
+        myUrls.add(url);
+        user.setMyUrls(myUrls);
+        UsersServices.getInstance().update(user);
+        return url;
+    }
 }
